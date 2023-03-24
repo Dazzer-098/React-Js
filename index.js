b9aeb8a20321acb693dcf89a8e1874e842629c4c
Rahul Vikram Chauhan <darcycric5300@gmail.com>
2023-03-23 23:12:11 -1200

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const Parent = React.createElement(
+  "div",
+  { id: "parent" },
+
+  React.createElement(
+    "div",
+    { id: "child" },
+
+    React.createElement("h1", {}, "hello react world")
+  )
+);
+
+const parent2 = React.createElement(
+  "div",
+  { id: "parent2" },
+
+  React.createElement(
+    "div",
+    { id: "child2" },
+
+    [
+      React.createElement(
+        "h1",
+        { className: "child12", key: "12" },
+        "hello react2 world"
+      ),
+      React.createElement("h1", {}, "hello react2 world"),
+    ]
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(Parent);
+console.log(parent2);
+const root1 = ReactDOM.createRoot(document.getElementById("root1"));
+root1.render(parent2);
+
+// const heading= document.createElement("h1");
+// heading.innerHTML="hello"
+// const root=document.body.appendChild(heading);
+// console.log(heading);
